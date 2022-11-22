dd91c55b0646c7064497c02028382a75a0e625cd
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-11-23 00:31:38 +0530

    Initial commit,Added 4 projects

---
+++ leapYear.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int year;
+    cout<<"Enter the year: "<<flush;
+    cin>> year;
+    if(year%400 == 0){
+        cout<<year<<" is a leapyear"<<endl;
+    }else if(year%100 == 0){
+        cout<<year<<" is not a leapyear"<<endl;
+    }else if(year%4 == 0){
+        cout<<year<<" is a leapyear"<<endl;
+    }else{
+        cout<<year<<" is not a leapyear"<<endl;
+    }
+    return 0;
+}
---
+++ oddOrEvenUsingOOPs.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class object{
+    private:
+        int number;
+    public:
+        void oddOrEven(int n){
+            if(n%2 == 0){
+                cout << n <<" is even" << endl;
+            }else{
+                cout << n <<" is odd" << endl;
+            }
+        }
+
+};
+
+int main(){
+    object o;
+    int number;
+    cout << "Enter a number" << endl;
+    cin >> number;
+    o.oddOrEven(number);
+
+    return 0;
+}
