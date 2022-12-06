ad71c5c44975097f7470cfb436475e4e0180ad03
Soumyajit Paria <soumyajitparia2005@gmail.com>
2022-12-06 21:54:06 +0530

    Added 1 more program

---
+++ employeeData.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class employee{
+    private:
+    string eName;         // name of an employee
+    float eSalary;        // salary of an employee
+    int eAge;             // age of an employee
+    int calcYearlySalary(int salary){
+        int yearlySalary = ((salary*12) + ((salary*12*15)/100));
+        return yearlySalary;
+    };  // Method to calculate yearly salary of an employee
+
+    public:
+    void setEmpData(){
+        cout<<"Enetr your name: "<<endl;
+        cin>>eName;
+        cout<<"Enetr your monthly salary: "<<endl;
+        cin>>eSalary;
+        cout<<"Enetr your age: "<<endl;
+        cin>>eAge;
+    };
+    void displayImpData(){
+        cout<<"Your name is "<<eName<<".you are "<<eAge<<" years old."<<" And your Yearly salary is "<<calcYearlySalary(eSalary)<<endl;
+    };
+};
+
+int main(){
+    employee employee1;
+    employee1.setEmpData();
+    employee1.displayImpData();
+    return 0;
+}
